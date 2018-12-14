fcc159cc9378c3fb20c9de21e849b4d374b9aabc
Dmitry Zaitsev <hhrhhr@gmail.com>
2018-12-15 01:01:36 +0300

    Update x4_parse_lang.lua

--- lua/x4_parse_lang.lua
+++ lua/x4_parse_lang.lua
@@ -1,65 +1,59 @@
 local conf = require("x4_config")
+local l_dir = conf.res_dir .. "/t/"
 
-arg[1] = conf.res_dir .. "/t/" .. conf.lang .. ".xml"
-arg[2] = conf.lang .. ".lua"
+local lang = { "07", "33", "34", "39", "44", "49", "55", "81", "82", "86", "88" }
 
---[[
-local mt = {}
-function mt:__index(key)
-    print("!!! index " .. key .. " not found.")
---    os.exit()
-    return "#" .. key .. "#"
-end
-setmetatable(WARE, mt)
---]]
 
 --[[ ]]------------------------------------------------------------------------
 
 local xml2lua = require("xml2lua")
 local handler = require("xmlhandler.tree")
-local parser = xml2lua.parser(handler)
 
-parser.options.stripWS = true
-parser.options.expandEntities = false
-parser.options.errorHandler = function(err, pos) print("\n", err .. "\n", pos .. "\n") end
-
-local xml
-local f, e = io.open(arg[1], "rb")
-if f then
-    xml = f:read("a")
-    f:close()
-else
-    io.stderr:write(e, "\n")
-    xml = ""
-end
-parser:parse(xml)
-
-local out = assert(io.open(arg[2], "wb"))
-
+local out
 local function wr(k, v)
     out:write("[" .. k .. "]=[=[" .. v .. "]=],\n")
 end
 
-local l = handler.root.language
-out:write("local lang={\n")
-
-local page = l.page
-for i = 1, #page do
-    local p = page[i]
-    local pid = p._attr.id
-    out:write("-- page\n[" .. pid .. "]={\n")
-    local t = p.t
-    if #t > 1 then
-        for j = 1, #t do
-            local tj = t[j]
-            wr(tj._attr.id, tj[1])
-        end
+for i = 1, #lang do
+    print("lang:", lang[i])
+    local xml
+    local f, e = io.open(l_dir .. "0001-L0" .. lang[i] .. ".xml", "rb")
+    if f then
+        xml = f:read("a")
+        f:close()
     else
-        wr(t._attr.id, t[1])
+        io.stderr:write(e, "\n")
+        goto skip
     end
-    out:write("}, -- page[" .. pid .. "]\n")
+    local h = handler:new()
+    local parser = xml2lua.parser(h)
+    parser.options.stripWS = true
+    parser.options.expandEntities = false
+    parser.options.errorHandler = function(err, pos) print("\n", err .. "\n", pos .. "\n") end
+    parser:parse(xml)
+
+    out = assert(io.open("0001-L0" .. lang[i] .. ".lua", "wb"))
+    local l = h.root.language
+    out:write("local lang={\n")
+
+    local page = l.page
+    for i = 1, #page do
+        local p = page[i]
+        local pid = p._attr.id
+        out:write("-- page\n[" .. pid .. "]={\n")
+        local t = p.t
+        if #t > 1 then
+            for j = 1, #t do
+                local tj = t[j]
+                wr(tj._attr.id, tj[1])
+            end
+        else
+            wr(t._attr.id, t[1])
+        end
+        out:write("}, -- page[" .. pid .. "]\n")
+    end
+    out:write("} -- lang\nreturn lang\n")
+    out:close()
+
+    ::skip::
 end
-out:write("} -- lang\nreturn lang\n")
-
-out:close()
-
